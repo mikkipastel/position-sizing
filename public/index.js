27cf111cf72b2eb1d7a0f337936c02c301c6ff11
Glitch (glitch-in-bio) <none>
2023-05-10 19:04:57 +0000

    ⛱🦀 Checkpoint ./public/index.js:1062301/1124 ./layout/head.html:1062301/2271

---
+++ public/index.js
@@ -0,0 +1,9 @@
+if (location.protocol == "http:") location.protocol = "https:";
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("service-worker.js")
+    .then((reg) => console.log("Service Worker registered", reg))
+    .catch((err) => console.error("Service Worker **not** registered", err));
+} else {
+  console.warn("Service Worker not supported in this browser");
+}
