057851d97414543d2abfe4e031b5b816ac4b61db
Glitch (glortch-hello-installable) <none>
2023-07-16 11:35:28 +0000

    🚌🏔 Checkpoint ./public/calculate.js:43247026/714

--- public/calculate.js
+++ public/calculate.js
@@ -1,3 +1,23 @@
-function getTakeProfit(entry_price) {
-  //
+/* HTML */
+getElementValue("inputCoinMame");
+
+function getElementValue(id) {
+  var element = document.getElementById(id);
+  var idValue = element.value;
+  console.log(idValue);
+}
+
+/* Formular */
+
+function getTakeProfit(entryPrice, stopLossPrice, multiple) {
+  return entryPrice + (multiple * (entryPrice - stopLossPrice));
+}
+
+function getAmountBuy(maxLoss, entryPrice, stopLossPrice) {
+  return maxLoss / Math.abs(entryPrice - stopLossPrice);
+}
+
+function getUsdUses(maxLoss, entryPrice, stopLossPrice, leverage) {
+  var amountBuy = getAmountBuy(maxLoss, entryPrice, stopLossPrice);
+  return amountBuy * entryPrice / leverage
 }
