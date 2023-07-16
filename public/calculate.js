f98ea2c21c5131f84c252b9d2c1f8751f2111fc9
Glitch (glortch-hello-installable) <none>
2023-07-16 13:42:40 +0000

    🛬🏉 Checkpoint ./public/calculate.js:43247026/36

--- public/calculate.js
+++ public/calculate.js
@@ -45,6 +45,7 @@ function getLossPerOrder(startFund, riskPercentPerOrder) {
 function getTakeProfit(entryPrice, stopLossPrice, multiple) {
   return entryPrice + (multiple * (entryPrice - stopLossPrice));
 }
+//=IFERROR((E15+(3*(E15-E16))),"-")
 
 function getAmountBuy(maxLoss, entryPrice, stopLossPrice) {
   return maxLoss / Math.abs(entryPrice - stopLossPrice);
